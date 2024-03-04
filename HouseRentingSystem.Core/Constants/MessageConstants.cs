5131ea5477ced65b8cfa817e60afe317b9029cd1
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-03-04 11:27:58 +0200

    Become method implemented

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -3,6 +3,11 @@
     public static class MessageConstants
     {
         public const string RequiredError = "The field {0} is required";
+
         public const string StringLengthError = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone number already exists. Enter another one!";
+
+        public const string UserRentsExist = "You should have no rents to become an agent";
     }
 }
