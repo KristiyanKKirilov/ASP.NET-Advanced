ebefbd6755f6c8b1ff54575e461237e20f9a7841
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-03-04 10:03:19 +0200

    BecomeAgentModel created

--- HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,18 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants.Agent;
+
+namespace HouseRentingSystem.Core.Models.Agent
 {
 	public class BecomeAgentFormModel
 	{
-	}
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(
+            PhoneNumberMaxLength,
+            MinimumLength = PhoneNumberMinLength,
+            ErrorMessage = StringLengthError)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
+    }
 }
