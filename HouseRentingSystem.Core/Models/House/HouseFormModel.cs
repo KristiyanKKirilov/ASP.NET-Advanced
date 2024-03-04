d490b5794e4b9f6a097ade40d40cf8b7a079ba57
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-03-04 18:22:03 +0200

    HouseController and Model modified

--- HouseRentingSystem.Core/Models/House/HouseFormModel.cs
+++ HouseRentingSystem.Core/Models/House/HouseFormModel.cs
@@ -1,6 +1,46 @@
-﻿namespace HouseRentingSystem.Core.Models.House
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants.House;
+
+namespace HouseRentingSystem.Core.Models.House
 {
-	public class HouseFormModel
-	{
-	}
+    public class HouseFormModel
+    {
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(TitleMaxLength,
+            MinimumLength = TitleMinLength,
+            ErrorMessage = StringLengthError)]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(AddressMaxLength,
+            MinimumLength = AddressMinLength,
+            ErrorMessage = StringLengthError)]
+        public string Address { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(DescriptionMaxLength,
+             MinimumLength = DescriptionMinLength,
+            ErrorMessage = StringLengthError)]
+
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [Range(typeof(decimal), MinPricePerMonth, 
+            MaxPricePerMonth, 
+            ConvertValueInInvariantCulture = true,
+            ErrorMessage = PriceRangeError)]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }  
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<HouseCategoryServiceModel> Categories { get; set; } = new List<HouseCategoryServiceModel>();
+
+    }
 }
--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+});
 
 builder.Services.AddApplicationServices();
 
